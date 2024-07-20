8fcdee76e9d7c51f2e6954efb61bebabe26c2069
EC2 Default User <ec2-user@ip-172-31-87-81.ec2.internal>
2024-07-20 10:08:24 +0000

    mixing repo

---
+++ mixing.sh
@@ -0,0 +1,6 @@
+1. add 500grams of flour to a pan
+2. add one tablespoon of yeast
+3. add a pinch of salt
+4. add a cup of water to the mix 
+5. mix the ingredients thoroughly
+6. Knead to get a fine mix 
